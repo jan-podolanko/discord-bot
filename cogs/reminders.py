b6375c255070781780dd294cd4494227639bb2ea
Jan Podolanko <jan.podolanko11@gmail.com>
2022-02-23 17:18:40 +0100

    cleanup

--- cogs/reminders.py
+++ cogs/reminders.py
@@ -10,7 +10,7 @@ class Reminders(commands.Cog):
         self.scheduler = AsyncIOScheduler(timezone="Europe/Berlin")
         self.scheduler.start()
 
-    #bot sends number of days since houseki no kuni hiatus :(
+    #sends number of days since houseki no kuni hiatus :(
     @commands.command()
     async def hiatus(self, ctx):
         hiatus = date(2020,12,25)
@@ -18,31 +18,29 @@ class Reminders(commands.Cog):
         await ctx.send("It has been {} days since Land of the Lustrous went on hiatus. 😭".format(delta.days))
         await ctx.send(file=discord.File('./pics/sad.jpg'))
 
-    #bot gives current date and time
-    @commands.command()
-    async def time_now(self, ctx):
-        today = datetime.now()
-        date_time = today.strftime("%d/%m/%Y, %H:%M:%S")
-        await ctx.send(date_time)
-
+    #function neccessary to send reminder messages
     async def rem(self,context,message):
         await context.send(f'Reminder: "{message}"')
 
+    #sets up periodic reminders
     @commands.command()
     async def remind(self, ctx, msg, time, week_day=None, day_of_month=None):
         h,m = time.split(':')
         self.scheduler.add_job(self.rem,'cron', args=[ctx,msg], name=f"{msg} on {time}", day=day_of_month, day_of_week=week_day, hour=int(h), minute=int(m))
 
+    #sets up a single reminder at specific date
     @commands.command()
     async def remind_once(self, ctx, msg, time, date):
         h,m = [int(i) for i in time.split(':')]
         da,mo,ye = [int(i) for i in date.split('.')]
         self.scheduler.add_job(self.rem,'date', args=[ctx,msg], name=f"{msg} on {date}, {time}", run_date=datetime(ye,mo,da,h,m,0))
 
+    #sends list of reminders
     @commands.command()
     async def list(self,ctx):
         await ctx.send(self.scheduler.get_jobs())
     
+    #removes reminders by id
     @commands.command()
     async def remove(self,ctx,id):
         self.scheduler.remove_job(id)
